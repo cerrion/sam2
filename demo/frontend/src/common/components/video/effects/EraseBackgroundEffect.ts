9afa60f3fe97de0e7fc923794bc9ded6732bf382
Matteo Destro <matteo@cerrion.com>
2025-03-04 16:16:00 +0000

    Use default variants

--- demo/frontend/src/common/components/video/effects/EraseBackgroundEffect.ts
+++ demo/frontend/src/common/components/video/effects/EraseBackgroundEffect.ts
@@ -20,6 +20,7 @@ import {AbstractEffect, EffectFrameContext} from './Effect';
 export default class EraseBackgroundEffect extends AbstractEffect {
   constructor() {
     super(3);
+    this.variant = 2;
   }
 
   apply(
