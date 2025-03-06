1e9e1ad1a2f872e86c4fa158788d9de754841500
Matteo Destro <matteo@cerrion.com>
2025-03-06 16:21:12 +0000

    Revert https

--- demo/backend/server/app.py
+++ demo/backend/server/app.py
@@ -27,7 +27,7 @@ from strawberry.flask.views import GraphQLView
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-cors = CORS(app, origins="https://10.4.0.28:7262", supports_credentials=True)
+cors = CORS(app, supports_credentials=True)
 
 videos = preload_data()
 set_videos(videos)
